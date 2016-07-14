9d72be9397ee14266a4498369dee7d6f8e846850
Thomas Minshull <thomasminshull@gmail.com>
2016-07-14 11:29:36 -0700

    changed to landscape and created a customLayoutClass

---
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.m
+//  CoverFlowLayout
+//
+//  Created by thomas minshull on 2016-07-14.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import "CoverFlowLayout.h"
+
+@implementation CoverFlowLayout
+
+@end

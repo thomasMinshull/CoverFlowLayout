87309cc2fb75bcf1b8883cc36f17ef06e21c196f
Thomas Minshull <thomasminshull@gmail.com>
2016-07-14 11:19:43 -0700

    initial commit

---
+++ CoverFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoverFlowLayout
+//
+//  Created by thomas minshull on 2016-07-14.
+//  Copyright © 2016 thomas minshull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
